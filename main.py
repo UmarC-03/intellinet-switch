c44a5c9704368c806818c25e4161849ae9e98dfc
UmarC-03 <umarcoovadia@gmail.com>
2026-03-22 23:38:30 +0200

    feat: add user configuration and state-aware switching logic

--- main.py
+++ main.py
@@ -1,28 +1,43 @@
 import random
 import time
 
+# --- Configuration ---
+print("====== INTELLINET SWITCH ======")
+intervel = int(input("Enter interval (seconds): "))
+threshold = int(input("Enter latency threshold (ms): "))
+current_network = str(input("Enter current network (WiFi/Hotspot): ")).strip().capitalize()
+print(current_network)
+print("================================")
+
+
 def get_wifi_ping():
     return random.randint(40, 150)
 
 def get_hotspot_ping():
     return random.randint(50, 120)
 
-def switch_to_hotspot():
-    print("Switching to hotspot due to lower latency")
+def network_hotspot():
+    global current_network
+    current_network = "Hotspot"
 
-def stay_on_wifi():
-    print("Staying on WiFi")
+def network_wifi():
+    global current_network
+    current_network = "WiFi"
 
 while True:
     wifi_ping = get_wifi_ping()
     hotspot_ping = get_hotspot_ping()
 
-    print(f"WiFi: {wifi_ping}ms | Hotspot: {hotspot_ping}ms")
+    print(f"WiFi: {wifi_ping}ms | Hotspot: {hotspot_ping}ms | Current Network: {current_network}")
 
-    if wifi_ping - hotspot_ping > 20:
-        switch_to_hotspot()
-    else:
-        stay_on_wifi()
+    if current_network == "WiFi":
+        if wifi_ping - hotspot_ping > threshold:
+            network_hotspot()
+            print("Switching to Hotspot due to high latency.")
+    elif current_network == "Hotspot":
+        if hotspot_ping - wifi_ping > threshold:
+            network_wifi()
+            print("Switching to WiFi due to high latency.")
 
     print("-" * 30)
-    time.sleep(2)
+    time.sleep(intervel)
